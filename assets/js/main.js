9e790e3463528399527a8daced79b58422a7567e
HBKarim <find_yourlove2@yahoo.com>
2019-04-19 05:02:12 +0200

    commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,11 +2,15 @@
 $(document).ready(function(){
   // Height
   $('#cubeTransition').height($(window).height());
+  // Menu Button [ Show Side Menu ]
+  $('.menu_button').click(function(){
+    $('.sideBar').toggleClass('move_left');
+  });
   // Preloader
   $(document).ready(function(){
       $.fakeLoader({
           timeToHide:1200,
-          bgColor:"#3ea6ff",
+          bgColor:"#5fbec7",
           spinner:"spinner5"
       });
   });
